5e83378e238c0beb748ca88da7f1382a0d4942c1
hemantharajMunusamy <hmunusamy1@sheffield.ac.uk>
2025-10-19 00:59:18 +0100

    Run the demo 00 with boost/mpi implementation

---
+++ repast_tutorial/demo_00_boost_mpi/demo_00.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <boost/mpi.hpp>
+
+int main(int argc, char** argv) {
+
+    boost::mpi::environment env(argc, argv);
+    
+    boost::mpi::communicator world;
+
+    if(world.rank() == 1) {
+        printf("Hello, world! I'm rank %d \n", world.rank());
+    } else {
+        printf("Hmm...\n");
+    }
+}
